ec82fa02383ab7382187fe909d4562ca9e0131c8
oleh <olekeke999@gmail.com>
2026-05-22 13:07:44 +0300

    Fixed Package.swift to contain missed dependencies - PINCache and GCDWebServer.

--- Package.swift
+++ Package.swift
@@ -1,21 +1,43 @@
-// swift-tools-version:5.1
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
-  name: "HLSCachingReverseProxyServer",
-  platforms: [
-    .macOS(.v10_11), .iOS(.v9), .tvOS(.v9)
-  ],
-  products: [
-    .library(name: "HLSCachingReverseProxyServer", targets: ["HLSCachingReverseProxyServer"]),
-  ],
-  dependencies: [
-    .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
-    .package(url: "https://github.com/devxoul/SafeCollection.git", .upToNextMajor(from: "3.1.0")),
-  ],
-  targets: [
-    .target(name: "HLSCachingReverseProxyServer"),
-    .testTarget(name: "HLSCachingReverseProxyServerTests", dependencies: ["HLSCachingReverseProxyServer", "Nimble", "SafeCollection"]),
-  ]
+    name: "HLSCachingReverseProxyServer",
+    platforms: [
+        .macOS(.v10_11),
+        .iOS(.v12),
+        .tvOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "HLSCachingReverseProxyServer",
+            targets: ["HLSCachingReverseProxyServer"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
+        .package(url: "https://github.com/devxoul/SafeCollection.git", .upToNextMajor(from: "3.1.0")),
+        .package(url: "https://github.com/pinterest/PINCache", from: "3.0.4"),
+        .package(url: "https://github.com/readium/GCDWebServer", from: "4.0.1"),
+    ],
+    targets: [
+        .target(
+            name: "HLSCachingReverseProxyServer",
+            dependencies: [
+                .product(name: "PINCache", package: "PINCache"),
+                .product(name: "GCDWebServer", package: "GCDWebServer"),
+            ]
+        ),
+        .testTarget(
+            name: "HLSCachingReverseProxyServerTests",
+            dependencies: [
+                "HLSCachingReverseProxyServer",
+                "Nimble",
+                "SafeCollection",
+                .product(name: "PINCache", package: "PINCache"),
+                .product(name: "GCDWebServer", package: "GCDWebServer"),
+            ]
+        ),
+    ]
 )
